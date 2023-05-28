b1b3e3dd3944481ca17a05958575c1d74aa90756
Zan Owsley <zanowsley@gmail.com>
2023-05-28 11:12:46 -0600

    starting new emacs repo

---
+++ init.el
@@ -0,0 +1,200 @@
+(defmacro zan-package (package &rest args)
+  `(progn
+     (unless (package-installed-p ',package)
+       (package-install ',package))
+     (require ',package)
+     ,@args))
+
+
+;; Seperate out custom file
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error)
+
+
+;; Add a directory for init modules
+(add-to-list 'load-path "~/.emacs.d/init-modules")
+
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+
+;; Basic UI stuff
+
+(toggle-frame-fullscreen)
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(tooltip-mode -1)
+
+(global-display-line-numbers-mode t)
+(dolist (mode '(org-mode-hook
+		eshell-mode-hook
+		shell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode -1))))
+
+(setq visible-bell t)
+(setq vc-follow-symlinks t)
+(add-hook 'Info-mode-hook 'visual-line-mode) ; get word wrap on all help docs
+(setq initial-scratch-message nil)
+
+;; Autosave and backup
+(setq kill-buffer-delete-auto-save-files t)
+(setq make-backup-files nil)
+
+
+;; Initialize use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+;; Packages
+
+(use-package diminish)
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode)
+
+(which-key-setup-minibuffer) ; to help with echo area covering which-key
+
+(use-package treemacs
+  :defer t
+  :config
+  (setq treemacs-no-png-images t
+	  treemacs-width 24)
+  :bind ("C-c t" . treemacs))
+
+(use-package eglot
+  :defer t
+  :hook (python-mode . eglot-ensure))
+
+(require 'text-property-search) ; Potential short term workaround for eglot
+
+(use-package corfu
+  :custom
+  (corfu-auto t)
+  :init
+  (global-corfu-mode))
+
+(use-package pcmpl-args)
+
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :diminish git-gutter-mode
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
+(setenv "WORKON_HOME" "~/.venvs/")
+(use-package pyvenv)
+(define-key global-map (kbd "C-c v") 'pyvenv-workon)
+
+(use-package magit)
+(setq magit-repository-directories
+      '(("~/" . 1)
+	("~/codehome/python/projects/" . 1)))
+
+;; Themes
+(use-package ef-themes)
+(load-theme 'ef-cherie :no-confirm)
+
+
+;; To extend the authinfo hiding of passwords to other stuff
+(setq authinfo-hidden (rx (or "password"
+			      "client-id"
+			      "client-secret"
+			      "refresh-token")))
+
+
+
+(require 'zan-email)
+
+;; Org-mode-capture-templates
+
+(setq org-directory "~/orghome")
+(setq rmt "/ssh:zan@zan-desktop.freeddns.org:")
+
+(if (string= system-name "zan-desktop-linux")
+    (setq org-agenda-files '("~/orghome/")
+	  gtd "~/orghome/gtd.org")
+    (setq org-agenda-files `(,(concat rmt "orghome/"))
+	  gtd (concat rmt "orghome/gtd.org")))
+
+(define-key global-map (kbd "C-c c") 'org-capture)
+(define-key global-map (kbd "C-c a") 'org-agenda)
+
+(setq org-capture-templates
+      '(
+	("g" "Templates for gtd")
+	("gu"
+	 "Urgent"
+	 entry
+	 (file+headline gtd "Urgent")
+	 "* TODO %?")
+	("ga"
+	 "Anytime"
+	 entry
+	 (file+headline gtd "Anytime")
+	 "* TODO %?")
+	("gd"
+	 "Daytime"
+	 entry
+	 (file+headline gtd "Daytime")
+	 "* TODO %?")
+	("gp"
+	 "Personal"
+	 entry
+	 (file+headline gtd "Personal")
+	 "* TODO %?")
+	("gh"
+	 "Home"
+	 entry
+	 (file+headline gtd "Home")
+	 "* TODO %?")
+	("ge"
+	 "Errands"
+	 entry
+	 (file+headline gtd "Errands")
+	 "* TODO %?")
+	("gc"
+	 "Casi"
+	 entry
+	 (file+headline gtd "Casi")
+	 "* TODO %?")
+	("gD"
+	 "Dave"
+	 entry
+	 (file+headline gtd "Dave")
+	 "* TODO %?")
+	("gP"
+	 "Prot"
+	 entry
+	 (file+headline gtd "Prot")
+	 "* TODO %?")))
+
+
+(defun find-gtd ()
+  (interactive)
+  (find-file gtd))
+(define-key global-map (kbd "C-c g") 'find-gtd)
+
+	 
---
+++ zan-email.el
@@ -0,0 +1,60 @@
+(require 'sendmail)
+
+;; This is the MU4E email client
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e/")
+
+(require 'mu4e)
+
+(setq user-full-name "Zan Owsley")
+(setq shr-use-colors nil)
+(setq shr-use-fonts nil)
+(setq mu4e-completing-read-function 'completing-read)
+(setq mu4e-view-show-images t)
+(setq mu4e-get-mail-command "offlineimap")
+(setq mu4e-update-interval 300)
+
+(setq send-mail-function 'smtpmail-send-it)
+
+(setq mu4e-compose-context-policy "ask-if-none")
+(setq mu4e-contexts
+      `(,(make-mu4e-context
+	  :name "work"
+	  :enter-func (lambda () (mu4e-message "Entering WORK"))
+	  :leave-func (lambda () (mu4e-message "Leaving WORK"))
+	  :match-func (lambda (msg)
+			(when msg
+			  (or
+			  (mu4e-message-contact-field-matches
+			   msg :to "zowsley@electro-meters.com"))))
+	  :vars '((user-mail-address . "zowsley@electro-meters.com")
+		  (mu4e-sent-folder . "/work/Sent Items")
+		  (mu4e-drafts-folder . "/work/Drafts")
+		  (mu4e-trash-folder . "/work/Trash")
+		  (mu4e-refile-folder . "/work/Archive")
+		  (smtpmail-smtp-server . "localhost")
+		  (smtpmail-stream-type . nil)
+		  (smtpmail-smtp-service . 1025)
+		  (smtpmail-queue-mail . nil)
+		  ))
+	,(make-mu4e-context
+	  :name "home"
+	  :enter-func (lambda () (mu4e-message "Entering HOME"))
+	  :leave-func (lambda () (mu4e-message "Leaving HOME"))
+	  :match-func (lambda (msg)
+			(when msg
+			  (mu4e-message-contact-field-matches
+			   msg :to "zanowsley@gmail.com")))
+	  :vars `((user-mail-address . "zanowsley@gmail.com")
+		  (mu4e-sent-folder . "/personal/[Gmail].Sent Mail")
+		  (mu4e-drafts-folder . "/personal/[Gmail].Drafts")
+		  (mu4e-trash-folder . "/personal/[Gmail].Trash")
+		  (mu4e-refile-folder . "/personal/Archives")
+		  (smtpmail-default-smtp-server . "smtp.gmail.com")
+		  (smtpmail-smtp-server . "smtp.gmail.com")
+		  (smtpmail-stream-type . ssl)
+		  (smtpmail-smtp-service . 465)
+		  (smtpmail-queue-mail . nil)
+		  ))))
+
+
+(provide 'zan-email)

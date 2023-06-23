3a1128c31f516475468c78fa850aa5839f00c29d
Zan Owsley <zanowsley@gmail.com>
2023-06-22 20:09:43 -0600

    added zanf-scratch-buffer to zan-editing-functions.el

--- zan-editing-functions.el
+++ zan-editing-functions.el
@@ -1,8 +1,61 @@
 ;; General text editing functions.
 
-(defun zan-kill-to-end-of-buffer ()
+(defun zanf-kill-to-end-of-buffer ()
   (interactive)
   (kill-region (point) (point-max)))
+
+
+;; Start of function group for making scratch buffers.
+
+(defvar zanv-derived-majors ())
+
+
+(defun zanf-derived-majors--mapatoms-f (symbol)
+  (when (and (apply 'provided-mode-derived-p symbol zanv-derived-majors)
+	     (not (member symbol zanv-derived-majors)))
+    (setq mode-found t)
+    (add-to-list 'zanv-derived-majors symbol)))
+
+
+(defun zanf-derived-majors--get-modes ()
+  (let ((mode-found nil))
+    (mapatoms 'zanf-derived-majors--mapatoms-f)
+    (if mode-found
+	(zanf-derived-majors--get-modes))))
+
+
+(defun zanf-derived-majors (&rest modes)
+  "Search 'obarray' for derived major modes currently loaded in Emacs.
+
+Parent modes can be specified, or if none are given, defaults to prog-mode
+and text-mode. Returns a list of found major modes. Also updates variable
+'zanv-derived-majors' with returned list."
+  (let ((parent-modes (if modes modes '(prog-mode text-mode))))
+    (setq zanv-derived-majors parent-modes)
+    (zanf-derived-majors--get-modes)
+    (dolist (mode parent-modes zanv-derived-majors)
+      (setq zanv-derived-majors (remove mode zanv-derived-majors)))))
+
+
+(defun zanf-scratch-buffer--choose-major ()
+  (completing-read "Which major mode? (default: python-mode) "
+		   (zanf-derived-majors) nil nil nil nil "python-mode"))
+
+
+(defun zanf-scratch-buffer ()
+  "Create and switch to a new scratch buffer with the chosen major mode enabled.
+
+Completion is available for major modes currently loaded in Emacs according to
+the function 'zanf-derived-majors'. If you wish to use a major mode not yet
+loaded, you may specify it anyway (without completion)."
+  (interactive)
+  (let* ((mode (zanf-scratch-buffer--choose-major))
+	(scratch-name (format "*%s-scratch*"
+			     (substring mode 0 -5))))
+    (get-buffer-create scratch-name)
+    (switch-to-buffer scratch-name)
+    (funcall (intern-soft mode))))
+
 
 
 (provide 'zan-editing-functions)
--- zan-org-functions.el
+++ zan-org-functions.el
@@ -1,20 +1,20 @@
-;; Start of zan-org-refile-list-item and helpers
+;; Start of zanf-org-refile-list-item and helpers
 ;; Lets you move a list item as-is to another heading within the same file.
 
-(defvar zan-org-refile-last-stored-list-item nil)
+(defvar zanv-org-refile-last-stored-list-item nil)
 
-(defun zan-org-refile-store-list-item ()
+(defun zanf-org-refile-store-list-item ()
   (let ((beg (line-beginning-position))
         (end (line-end-position)))	
-    (setq zan-org-refile-last-stored-list-item (buffer-substring-no-properties beg end))
+    (setq zanv-org-refile-last-stored-list-item (buffer-substring-no-properties beg end))
     (delete-region beg (line-beginning-position 2))))
 
-(defun zan-org-refile-choose-heading ()
+(defun zanf-org-refile-choose-heading ()
   (completing-read "Choose a heading: "
 		   (org-map-entries
 		    #'(org-get-heading :no-tags :no-todo :no-cookie :no-comment))))
 
-(defun zan-org-refile-find-heading (target-heading)
+(defun zanf-org-refile-find-heading (target-heading)
   (goto-char (point-min))
   (re-search-forward (format "^\\*+\\( .*? \\| \\)%s" target-heading) nil nil 1)
   (goto-char (line-beginning-position))
@@ -24,15 +24,15 @@
 	(goto-char (point-max)))
     (widen)))
 
-(defun zan-org-refile-list-item ()
+(defun zanf-org-refile-list-item ()
   (interactive)
-  (let ((target (zan-org-refile-choose-heading)))
-    (zan-org-refile-store-list-item)
-    (zan-org-refile-find-heading target)
+  (let ((target (zanf-org-refile-choose-heading)))
+    (zanf-org-refile-store-list-item)
+    (zanf-org-refile-find-heading target)
     (insert "\n")
-    (insert zan-org-refile-last-stored-list-item)))
+    (insert zanv-org-refile-last-stored-list-item)))
 
-;; End of zan-org-refile-list-item
+;; End of zanf-org-refile-list-item
 
 
 (provide 'zan-org-functions)

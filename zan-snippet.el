966f357aee801b1c2deea0bdd9382c92530468ab
Zan Owsley <zanowsley@gmail.com>
2023-11-07 14:33:31 -0700

    Fixed zan-snippet.el

--- zan-snippet.el
+++ zan-snippet.el
@@ -12,7 +12,8 @@
   (goto-char start)
   (when placeholder
     (while (search-forward placeholder end t)
-      (replace-match input)))
+      (replace-match input)
+      (goto-char start)))
   (when (re-search-forward "~[^~]*~" end t)
     (let* ((placeholder (match-string 0))
 	   (input
@@ -29,6 +30,8 @@
     (indent-region start end)
     (zanf-snippet-replace-field start end nil nil)
     (goto-char end)))
+
+(setq zanv-snippet-dir "~/sync-general/emacs/snippets/")
 
 (define-key global-map (kbd "C-c M-s") 'zanf-snippet-insert-at-point)
 

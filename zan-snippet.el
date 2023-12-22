8738a0939f5a0b163bff99335e59a89273360655
Zan Owsley <zanowsley@gmail.com>
2023-12-22 12:34:52 -0700

    Actually fixed snippet point moving to end.

--- zan-snippet.el
+++ zan-snippet.el
@@ -1,3 +1,18 @@
+(defun zanf-snippet--indent-line ()
+  (interactive)
+  (let ((pstart (point)))
+    (indent-according-to-mode t)
+    (- (point) pstart)))
+
+(defun zanf-snippet--indent-all ()
+  (interactive)
+  (goto-char zanv-snippet--start)
+  (while (<= (point) zanv-snippet--end)
+    (let ((shift (zanf-snippet--indent-line)))
+      (setq zanv-snippet--end (+ zanv-snippet--end shift))
+      (setq zanv-snippet--start (+ zanv-snippet--start shift)))
+    (next-line 1)))
+
 (defun zanf-snippet--choose-snippet ()
   (concat
    zanv-snippet-dir
@@ -12,37 +27,36 @@
   (read-string
    (concat (substring placeholder 1 -1) ": ")))
 
-(defun zanf-snippet--replace-placeholder (placeholder input start)
-  (goto-char start)
+(defun zanf-snippet--replace-placeholder (placeholder input)
+  (goto-char zanv-snippet--start)
   (when (search-forward placeholder zanv-snippet--end t)
     (replace-match input)
     (setq zanv-snippet--end (+ zanv-snippet--end (- (length input) (length placeholder))))
     (zanf-snippet--replace-placeholder
      placeholder
-     input
-     start)))
+     input)))
 
-(defun zanf-snippet--has-next-placeholder (start)
-  (goto-char start)
+(defun zanf-snippet--has-next-placeholder ()
+  (goto-char zanv-snippet--start)
   (if (re-search-forward "~[^~]*~" zanv-snippet--end t) t nil))
 
 (defun zanf-snippet-insert-at-point ()
   (interactive)
-  (let* ((start (point))
-	 (snip (zanf-snippet--choose-snippet))
+  (setq zanv-snippet--start (point))
+  (let* ((snip (zanf-snippet--choose-snippet))
 	 (length (nth 1 (insert-file-contents snip))))
-    (setq zanv-snippet--end (+ start length))
-    (indent-region start zanv-snippet--end)
-    (while (zanf-snippet--has-next-placeholder start)
+    (setq zanv-snippet--end (+ zanv-snippet--start length))
+    (zanf-snippet--indent-all)
+    (while (zanf-snippet--has-next-placeholder)
       (let* ((placeholder (match-string 0))
 	     (input (zanf-snippet--replace-placeholder-get-input placeholder)))
-	(zanf-snippet--replace-placeholder placeholder input start))))
+	(zanf-snippet--replace-placeholder placeholder input))))
   (goto-char zanv-snippet--end))
 
-(save-excursion)
 
 (setq zanv-snippet-dir "~/sync-general/emacs/snippets/")
 
 (define-key global-map (kbd "C-c M-s") 'zanf-snippet-insert-at-point)
 
 (provide 'zan-snippet)
+

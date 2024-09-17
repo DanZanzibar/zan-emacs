56529325770fdd71c889e3c1bba049cacfafdc90
Zan Owsley <zanowsley@gmail.com>
2024-09-17 17:24:20 -0600

    Added c-mode comile and run function

--- zan-c.el
+++ zan-c.el
@@ -4,6 +4,14 @@
   (let ((file (file-name-nondirectory buffer-file-name)))
     (compile (concat "gcc " file " -o " (file-name-sans-extension file)))))
 
+(defun zanf-compile-and-run-c ()
+  "Runs the output of the current buffer. Compiles if necessary."
+  (interactive)
+  (let* ((file (file-name-nondirectory buffer-file-name))
+	 (exe (file-name-sans-extension file)))
+    (compile (conact "gcc " file " -o " exe " && ./" exe))))
+
 (keymap-set c-mode-map "C-c c" 'zanf-compile-c)
+(keymap-set c-mode-map "C-c r" 'zanf-compile-and-run-c)
 
 (provide 'zan-c)

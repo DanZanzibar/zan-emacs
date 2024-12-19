2f5144ef1721a084e7b07b008bbed1d16314e98e
Zan Owsley <zanowsley@gmail.com>
2024-12-19 09:42:36 -0700

    ...

--- zan-ui.el
+++ zan-ui.el
@@ -3,9 +3,9 @@
 
 ;; Self explanatory.
 (setq inhibit-startup-message t)
-(set-scroll-bar-mode nil)
-(if (fboundp tool-bar-mode)
-    (tool-bar-mode -1))
+(when (boundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(tool-bar-mode -1)
 (menu-bar-mode -1)
 (tooltip-mode -1)
 (setq visible-bell t)

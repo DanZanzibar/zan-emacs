99f41ffba26a949fb04b1a388086384eaacdd32b
Zan Owsley <zanowsley@gmail.com>
2024-12-19 09:52:51 -0700

    still trying

--- zan-ui.el
+++ zan-ui.el
@@ -1,12 +1,12 @@
 ;;;; All basic UI customization goes here.
 
 
-;;; Self explanatory.
+;;; Basic UI customization.
+(require 'scroll-bar)
+(require 'tool-bar)
 (setq inhibit-startup-message t)
-(when (boundp 'scroll-bar-mode)
-  (scroll-bar-mode -1))
-(when (boundp 'tool-bar-mode)
-  (tool-bar-mode -1))
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
 (menu-bar-mode -1)
 (tooltip-mode -1)
 (setq visible-bell t)

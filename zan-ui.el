1335130f74884c460ae5ab64af68e97b55dff2bb
Zan Owsley <zanowsley@gmail.com>
2024-12-19 09:46:22 -0700

    Think I've fixed the bug

--- zan-ui.el
+++ zan-ui.el
@@ -1,11 +1,12 @@
-;; All basic UI customization goes here.
+;;;; All basic UI customization goes here.
 
 
-;; Self explanatory.
+;;; Self explanatory.
 (setq inhibit-startup-message t)
 (when (boundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
-(tool-bar-mode -1)
+(when (boundp 'tool-bar-mode)
+  (tool-bar-mode -1))
 (menu-bar-mode -1)
 (tooltip-mode -1)
 (setq visible-bell t)
